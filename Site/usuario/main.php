c0e2f5782b8c510cad05f12cbec37f5f16e6a5bc
Mateus V <49762931+CreatorMA@users.noreply.github.com>
2020-10-07 22:49:39 -0300

    adição branch mateus (1.5/2)

--- Site/usuario/main.php
+++ Site/usuario/main.php
@@ -21,30 +21,30 @@ Class User{
 
     public function login($email,$senha){
    
-            $sql = "Select codUsuario from Usuario where email = :email and senha = :senha";
+             $sql = "Select codUsuario from Usuario where email = :email and senha = :senha";
    
-            $stmt = $this->pdo->prepare($sql); 
-            $stmt->bindParam(":email", $email, PDO::PARAM_STR); 
-            $stmt->bindParam(":senha", $senha, PDO::PARAM_STR); 
-            $stmt->execute();
+             $stmt = $this->pdo->prepare($sql); 
+             $stmt->bindParam(":email", $email, PDO::PARAM_STR); 
+             $stmt->bindParam(":senha", $senha, PDO::PARAM_STR); 
+             $stmt->execute();
    
-            $login = $stmt->fetchAll();
-            if(count($login) > 0)
-            return $login[0]["codUsuario"];
+             $login = $stmt->fetchAll();
+             if(count($login) > 0)
+             return $login[0]["codUsuario"];
                
-            else
-            return "0";
+             else
+             return "0";
 
     }
 
    public function cadastro($nome,$email,$senha,$dataNasc,$CPF,$genero,$telefone){
 
-    $teste = str_split($nome,1);
-    $noNum = true;
+     $teste = str_split($nome,1);
+     $noNum = true;
     foreach($teste as $i){
-        if(is_numeric($i))
-        $noNum = false;
-    }
+         if(is_numeric($i))
+         $noNum = false;
+     }
     if($noNum){
          $sql = 'INSERT INTO Usuario(nomeUsuario,email,senha,dataNasc,CPF,genero,telefone) 
          VALUES(:nomeUsuario,:email,:senha,:dataNasc,:CPF,:genero,:telefone)';
