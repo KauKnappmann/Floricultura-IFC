fc770685f656605e8e67c424a9ed5b1ac407f50c
Mateus V <49762931+CreatorMA@users.noreply.github.com>
2020-10-09 14:50:07 -0300

    feedback login

--- Site/classes/user.php
+++ Site/classes/user.php
@@ -22,11 +22,11 @@ try{
          
          $_SESSION['login'] = $obj->login($_POST['email'],$_POST['senha']); 
 
-         echo "Bem-vindo(a) ".$_POST['nome']." ".$_POST['sobrenome'];
+         
          
         break;
 
-         case 1; //cadastro
+        case 1; //cadastro
 
          if(!isset($_SESSION))
         session_start();
@@ -35,23 +35,23 @@ try{
          $cad = $obj->cadastro( $_POST['nome'],$_POST['sobrenome'],$_POST['email'],$_POST['senha'],$_POST['dataNasc'],$_POST['genero'],
                                 $_POST['CPF'],$_POST['telefone'], $_POST['imagem']);
                             
-        if($cad != "OK!"){
+         if($cad != "OK!"){
             // descomente isso para achar erros ->
          //   var_dump($cad); 
             
             $obj->mensagemErro($cad);
          
-        }
+         }
         
-        $_SESSION['login'] = $obj->login($_POST['email'],$_POST['senha']); 
-            echo "Bem-vindo(a) ".$_POST['nome']." ".$_POST['sobrenome'];
+         $_SESSION['login'] = $obj->login($_POST['email'],$_POST['senha']); 
+            
          break;
 
 
 
     }
     //comente isso para procurar erros:
-   // header('location:../index.php');
+    header('location:../index.php');
 
 
    // quando for procurar busgs, lembre-se de olhar o banco de dados no MySql, tem algumas coisas como limite de caracteres que pode 
--- Site/index.php
+++ Site/index.php
@@ -4,11 +4,28 @@ $pdo = Conexao::getInstance();
 
 
 
-if(!isset($_SESSION)){
-    session_start();    
-    echo $_SESSION['login'];
-}else
-echo "n foi";
+if(!isset($_SESSION))
+    session_start();   
+    
+    if(isset($_SESSION['login'])){
+    $_SESSION['login'];
+    
+    $sql = "Select nome,sobrenome from Usuario where codUsuario = :cod";
+
+    $stmt = $pdo->prepare($sql); 
+    $stmt->bindParam(":cod", $_SESSION['login'], PDO::PARAM_STR); 
+    $stmt->execute();
+
+    $login = $stmt->fetchAll();
+
+echo "Bem vindo usuario ".$login[0]['nome'];
+}
+
+
+$sql = "Select codUsuario from Usuario where email = :email and senha = :senha";
+   
+
+
 //echo $_SESSION["login"];
 //header("location:index.html");
 ?>
