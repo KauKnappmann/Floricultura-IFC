1e39e980bc43ab70cd5c5650ef68eecc5e3f637b
alessandra-silva <alessandra-sa@hotmail.com>
2020-11-05 14:59:35 -0300

    feat (phpunit): user tests

--- Site/tests/UserTests.php
+++ Site/tests/UserTests.php
@@ -2,4 +2,12 @@
 
 USE PHPUnit\Framework\TestCase;
 
+require $_SERVER['DOCUMENT_ROOT'] . 'conf/Conexao.php';
+
+require "../classes/adm.php";
+
+$adm = new Adm(Conexao::getInstance());
+
+$adm->view(0);
+
 ?>
