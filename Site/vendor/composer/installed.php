0e96656e832fe688efc1b79825216e36f4f19330
alessandra-silva <alessandra-sa@hotmail.com>
2021-02-17 19:23:42 -0300

    TASK0153 fix composer e autoload

---
+++ Site/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5aaade292329f046d86f952556e95d21ee568e04',
+    'name' => 'kananak/floricultura-ifc',
+  ),
+  'versions' => 
+  array (
+    'kananak/floricultura-ifc' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5aaade292329f046d86f952556e95d21ee568e04',
+    ),
+  ),
+);
